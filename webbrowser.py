06891df73995130eef6e9133b7dad5fdcb588d4f
GalaxyShadow99 <thomas.constantin27@orange.fr>
2024-04-01 14:59:12 +0200

    Create webbrowser.py

---
+++ webbrowser.py
@@ -0,0 +1,10 @@
+import webview
+
+if __name__ == '__main__':
+    screens = webview.screens
+    print('Available screens are: ' + str(screens))
+
+    for i, screen in enumerate(screens):
+        webview.create_window('', html=f'placed on the monitor {i+1}', screen=screen)
+
+    webview.start()
